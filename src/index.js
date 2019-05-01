d4a82d416f1581d511146a1d1187bbd4e53b6329
Stuart Gill <stuart.a.gill@gmail.com>
2019-05-01 16:30:51 -0700

    initial commit, halfway through tutorial 3

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import uuidv4 from "uuid/v4";
+import "dotenv/config";
+import cors from "cors";
+import bodyParser from "body-parser";
+import express from "express";
+import models from "./models";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
+});
+
+app.get("/session", (req, res) => {
+  return res.send(req.context.models.users[req.context.me.id]);
+});
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(req.context.models.users));
+});
+
+app.get("/users/:userId", (req, res) => {
+  return res.send(req.context.models.users[req.params.userId]);
+});
+
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(req.context.models.messages));
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  return res.send(req.context.models.messages[req.params.messageId]);
+});
+
+app.post("/messages", (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.context.me.id
+  };
+
+  req.context.models.messages[id] = message;
+
+  return res.send(message);
+});
+
+app.delete("/messages/:messageId", (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = req.context.models.messages;
+
+  req.context.models.messages = otherMessages;
+
+  return res.send(message);
+});
+
+app.listen(3000, () =>
+  console.log(`this app is listening on port ${process.env.PORT}`)
+);
+
+//console.log("hello eternally running node.js project");
+
+//console.log(process.env.MY_SECRET);
